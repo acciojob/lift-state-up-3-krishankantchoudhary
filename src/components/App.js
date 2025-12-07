3618232eb9f09adbe582bf37ee67c9a4d92bada9
krishan kant choudhary <83417448+krishankantchoudhary@users.noreply.github.com>
2025-12-07 21:54:36 +0530

    reupload

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React, { useState } from "react";
 import './../styles/App.css';
+import ChildComponent1 from "./ChildComponent1";
+import ChildComponent2 from "./ChildComponent2";
 
 const App = () => {
+  const[selectedOption, setSelectedOption]=useState("");
+
+  const handleselect=(optionvalue)=>{
+    setSelectedOption(optionvalue)
+  }
+
   return (
-    <div>
-        {/* Do not remove the main div */}
+    <div style={{backgroundColor:"#32CD32"}}>
+        <h1>Parent Component</h1>
+        <ChildComponent1 onSelect={handleselect}/>
+        <ChildComponent2 onSelect={handleselect}/>
+        <p>Selected Option: {selectedOption}</p>
     </div>
   )
 }
